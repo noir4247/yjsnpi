a7fbbfc6d31e014548f48bd65ccb7a365d247155
noir4247 <60754335+noir4247@users.noreply.github.com>
2020-07-08 23:11:32 +0900

    Add files via upload
    et_cetera_lab

---
+++ et_cetera_lab/main.js
@@ -0,0 +1,19 @@
+function clk_home()
+{
+    document.getElementById("content").innerHTML = '<H2>えとせとら研究所のトップページ</H2>';
+}
+
+function clk_news()
+{
+    document.getElementById("content").innerHTML = '';
+}
+
+function clk_contact()
+{
+    document.getElementById("content").innerHTML = '<p>Twitter: <a href="https://twitter.com/et_cetera_lab?s=20">@et_cetera_lab</a></p>';
+}
+
+function clk_about()
+{
+    document.getElementById("content").innerHTML = 'えとせとら研究所';
+}
